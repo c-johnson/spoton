ef32abcb726d69c27d38bdb0339baf7fa8032696
Chris Johnson <chris@cjohnson.io>
2015-02-04 14:46:01 -0800

    Toggle button working properly  There is an issue in AngularStrip which prevents the model property from updating when radio buttons are clicked.  I hacked a solution to this, but I’m not happy with it (the $scope.activate function on the HomeCtrl)

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,11 @@
 var spoton = angular.module('spoton');
 
 spoton.controller('HomeCtrl', ['$http', '$scope', function ($http, $scope) {
-  $scope.toggleButton = "list";
+  $scope.toggleButton = 'list';
+
+  $scope.activate = function (active) {
+    $scope.toggleButton = active;
+  };
 
   $http.get('http://localhost:3000/events.json')
     .success(function(data) {
